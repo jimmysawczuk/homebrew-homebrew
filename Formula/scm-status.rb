8e506880a954c90979ce481cbe3c5a04bad6abcb
Jimmy Sawczuk <me@jimmysawczuk.com>
2021-03-10 19:36:25 -0500

    Add version

--- Formula/scm-status.rb
+++ Formula/scm-status.rb
@@ -3,11 +3,12 @@ class ScmStatus < Formula
   homepage "jimmysawczuk.github.io/scm-status"
   url "https://github.com/jimmysawczuk/scm-status/archive/v2.2.2.zip"
   sha256 "b69f791fc4360331e34686f7f93936d78a6a8abae0f3809f980a463f74bc16f3"
+  version "v2.2.2"
 
   depends_on "go" => :build
 
   def install
     mkdir bin
-    system "go", "build", "-trimpath", "-o", bin, "github.com/jimmysawczuk/scm-status/..."
+    system "go", "build", "-trimpath", "-ldflags", "-X main.version=#{version}", "-o", bin, "github.com/jimmysawczuk/scm-status/..."
   end
 end
