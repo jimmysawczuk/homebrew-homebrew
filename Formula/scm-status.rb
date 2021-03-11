c88ff0605fba61f00245d15bf026cddd3f5a4e36
Jimmy Sawczuk <me@jimmysawczuk.com>
2021-03-10 19:46:29 -0500

    Fix sha

--- Formula/scm-status.rb
+++ Formula/scm-status.rb
@@ -2,7 +2,7 @@ class ScmStatus < Formula
   desc "Repository status snapshot generator"
   homepage "jimmysawczuk.github.io/scm-status"
   url "https://github.com/jimmysawczuk/scm-status/archive/v2.2.3.zip"
-  sha256 "fa16a5d7f9041444b55c9ee61b24d6fc6ab83f2925145d2cd728457613143b2f"
+  sha256 "0e2a8d835af6ef1877ff622c1a6a274d31f35f05e82a4b4c35a100d694db4531"
   version "v2.2.3"
 
   depends_on "go" => :build
