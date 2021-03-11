5943ecba82ecbde98259e72d384ccf969b298184
Jimmy Sawczuk <me@jimmysawczuk.com>
2021-03-10 19:25:58 -0500

    Initial commit

---
+++ Formula/scm-status.rb
@@ -0,0 +1,13 @@
+class ScmStatus < Formula
+  desc "Repository status snapshot generator"
+  homepage "jimmysawczuk.github.io/scm-status"
+  url "https://github.com/jimmysawczuk/scm-status/archive/v2.2.2.zip"
+  sha256 "b69f791fc4360331e34686f7f93936d78a6a8abae0f3809f980a463f74bc16f3"
+
+  depends_on "go" => :build
+
+  def install
+    mkdir bin
+    system "go", "build", "-trimpath", "-o", bin, "github.com/jimmysawczuk/scm-status/..."
+  end
+end
