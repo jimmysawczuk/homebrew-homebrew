8d0240742dc06825df15a888db72601e13e56b5a
Jimmy Sawczuk <me@jimmysawczuk.com>
2021-03-13 21:26:37 -0500

    Bump version

--- Formula/scm-status.rb
+++ Formula/scm-status.rb
@@ -1,9 +1,9 @@
 class ScmStatus < Formula
   desc "Repository status snapshot generator"
   homepage "jimmysawczuk.github.io/scm-status"
-  url "https://github.com/jimmysawczuk/scm-status/archive/v2.2.3.zip"
-  sha256 "0e2a8d835af6ef1877ff622c1a6a274d31f35f05e82a4b4c35a100d694db4531"
-  version "v2.2.3"
+  url "https://github.com/jimmysawczuk/scm-status/archive/v2.3.0.zip"
+  sha256 "b6947fc9f0c65329ab05baf8fb39c6e17ca1ed5242981efeba888d25e8dac46c"
+  version "v2.3.0"
 
   depends_on "go" => :build
 
